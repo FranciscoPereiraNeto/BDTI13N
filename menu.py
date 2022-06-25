1268a706ddef08eba8504a5ea82618395f82c10b
FRANCISCO PEREIRA DE CARVALHO NETO <francisco.pcneto@senacsp.edu.br>
2022-06-24 21:32:18 -0300

    first commit

---
+++ menu.py
@@ -0,0 +1,66 @@
+import operacoes
+import this
+this.opcao = -1
+this.codigo = 0
+this.campo = ""
+
+def menu():
+    print('\nEscolha uma das opções abaixo: \n\n' +
+          '1. Cadastrar\n'                        +
+          '2. Consultar\n'                        +
+          '3. Atualizar nome\n'                   +
+          '4. Atualizar Telefone\n'               +
+          '5. Atualizar Endereco\n'               +
+          '6. Atualizar Data de Nascimento\n'     +
+          '7. Excluir\n'                          +
+          '0.Sair\n')
+    this.opcao = int(input())
+
+def operacao():
+    while(this.opcao != 0):
+        menu()
+        if this.opcao == 1:
+            print('Informe seu nome: ')
+            nome = input()
+            print('Informe seu telefone: ')
+            telefone = input()
+            print('Informe seu endereço: ')
+            endereco = input()
+            print('Informe sua data de nascimento: ')
+            dtNasc   = input()
+            #Chamar o método inserir
+            operacoes.inserir(nome, telefone, endereco, dtNasc)
+        elif this.opcao == 2:
+            operacoes.consultar()
+        elif this.opcao == 3:
+            print("Informe o código que deseja atualizar")
+            this.codigo=int(input())
+            print("Informe o novo nome: ")
+            this.nome = input()
+            operacoes.atualizar(this.codigo, 'nome', this.campo)
+        elif this.opcao == 4:
+            print("Informe o código que deseja atualizar")
+            this.codigo = int(input())
+            print("Informe o novo telefone: ")
+            this.nome = input()
+            operacoes.atualizar(this.codigo, 'telefone', this.campo)
+        elif this.opcao == 5:
+            print("Informe o codigo que deseja atualizar")
+            this.codigo = int(input())
+            print("Informe o novo endereco: ")
+            this.campo = input()
+            operacoes.atualizar(this.codigo, 'endereco', this.campo)
+        elif this.opcao == 6:
+            print("Informe o codigo que deseja atualizar")
+            this.codigo = int(input())
+            print("Informe o nova Data de nascimento: ")
+            this.campo = input()
+            operacoes.atualizar(this.codigo, 'dataDeNascimento', operacoes.tratarData(this.campo))
+        elif this.opcao == 7:
+            print("Informe o codigo que para exclusão do dado")
+            this.codigo = int(input())
+            operacoes.excluir(this.codigo)
+
+        else:
+            print('Opção escolhida não é válida!')
+
